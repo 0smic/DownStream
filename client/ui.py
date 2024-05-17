49989f274b25575f7ed341fb2e4447f5f961c443
Gokul <146622216+0smic@users.noreply.github.com>
2024-05-18 00:41:23 +0530

    Fixed GUI not responding issue
    Fixed GUI not closing after client got kick out.
    Added new Edit profile frame to edit profile pic and etc..

--- client/ui.py
+++ client/ui.py
@@ -17,16 +17,12 @@ logging.basicConfig(filename="app_log.log", level=logging.DEBUG, format='%(ascti
 
 HOST = "127.0.0.1"
 PORT = 9093
-
-
-
 SUCCESS = 0
 FAILED = 1
 INVAILD = 2
-
 WORKING = 0
 NOT_WORKING = 1
-
+COUNT = 0
 KICK_CODE = "323423439823HFD8F9834H33239U234JG3"
 BAN_CODE = "34IO34344HJKDF99ADFGUFJNKNDFA9ASFF"
 
@@ -53,7 +49,6 @@ class FirstSectionFrame(customtkinter.CTkFrame):
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-
     def switch_to_register(self):
         # Call the callback to switch to the Register frame
         self.switch_frame_callback("RegisterFrame")
@@ -63,39 +58,41 @@ class FirstSectionFrame(customtkinter.CTkFrame):
         self.switch_frame_callback("LoginFrame")
         logging.info("Switched to LoginFrame")
 
+
 class LoginFrame(customtkinter.CTkFrame):
     def __init__(self, master, switch_frame_callback):
         super().__init__(master)
         self.switch_frame_callback = switch_frame_callback
 
-        # Back to the main frame button
+        # Back icon
         back_image = Image.open("rc/back-icon.png")
         back_image = back_image.resize((10,10))
         back_icon = ImageTk.PhotoImage(back_image)
 
+        # Back to the main frame button
         self.back_button = customtkinter.CTkButton(self, image=back_icon,text="", border_width=0, command=self.switch_to_main,fg_color="#333333",width=0)
         self.back_button.image = back_icon
         self.back_button.grid(row=0, column=0, pady=(10, 0), sticky="w")
 
-        # Entry for the username
-
+        # Username Label
         self.username_label = customtkinter.CTkLabel(self, text="Username:")
         self.username_label.grid(row=3, column=0, pady=(10,0), padx=10, sticky="w")
 
+        # Username input box
         self.username_entry = customtkinter.CTkEntry(self)
         self.username_entry.grid(row=3,column=1, pady=(10,0), padx=10, sticky="e")
 
-        # Entry for the password
+        # Password Label
         self.password_label = customtkinter.CTkLabel(self, text="Password:")
         self.password_label.grid(row=4, column=0, pady=(10,0), padx=10, sticky="w")
 
+        # Password input box
         self.password_entry = customtkinter.CTkEntry(self, show="*")
         self.password_entry.grid(row=4,column=1, pady=(10,0), padx=10, sticky="e")
 
         # Login Button
         self.login_button = customtkinter.CTkButton(self, text="Login", command=self.login_user)
         self.login_button.grid(row=5, column=1, pady=(10,20), padx=10)
-
 
     def login_user(self):
         username = self.username_entry.get()
@@ -123,22 +120,22 @@ class LoginFrame(customtkinter.CTkFrame):
         self.switch_frame_callback("AfterLoginFrame")
         logging.info("Switched to AfterLoginFrame")
 
-
 class RegisterFrame(customtkinter.CTkFrame):
     def __init__(self, master, switch_frame_callback):
         super().__init__(master)
         self.switch_frame_callback = switch_frame_callback
 
-        # Back to the main frame button
+        # Back Icon
         back_image = Image.open("rc/back-icon.png")
         back_image = back_image.resize((10,10))
         back_icon = ImageTk.PhotoImage(back_image)
-
+        
+         # Back to the main frame button
         self.back_button = customtkinter.CTkButton(self, image=back_icon,text="", border_width=0, command=self.switch_to_main,fg_color="#333333",width=0)
         self.back_button.image = back_icon
         self.back_button.grid(row=0, column=0, pady=(10, 0), sticky="w")
 
-        #Taking Input
+        # Username Label
         self.username_label = customtkinter.CTkLabel(self, text="Username:")
         self.username_label.grid(row=3, column=0, pady=(10, 0), padx=10, sticky="w")
 
@@ -146,6 +143,7 @@ class RegisterFrame(customtkinter.CTkFrame):
         self.username_entry = customtkinter.CTkEntry(self)
         self.username_entry.grid(row=3, column=1, pady=(10, 0), padx=10, sticky="e")
 
+        # Name Label
         self.name_label = customtkinter.CTkLabel(self, text="Name:")
         self.name_label.grid(row=4, column=0, pady=(10, 0), padx=10, sticky="w")
 
@@ -153,6 +151,7 @@ class RegisterFrame(customtkinter.CTkFrame):
         self.name_entry = customtkinter.CTkEntry(self)
         self.name_entry.grid(row=4, column=1, pady=(10, 0), padx=10, sticky="e")
         
+        # Password Label
         self.password_label = customtkinter.CTkLabel(self, text="Password:")
         self.password_label.grid(row=5, column=0, pady=(10, 0), padx=10, sticky="w")
 
@@ -160,6 +159,7 @@ class RegisterFrame(customtkinter.CTkFrame):
         self.password_entry = customtkinter.CTkEntry(self)
         self.password_entry.grid(row=5, column=1, pady=(10, 0), padx=10, sticky="e")
 
+        # Email Label
         self.email_label = customtkinter.CTkLabel(self, text="Email:")
         self.email_label.grid(row=6, column=0, pady=(10, 0), padx=10, sticky="w")
 
@@ -167,6 +167,7 @@ class RegisterFrame(customtkinter.CTkFrame):
         self.email_entry = customtkinter.CTkEntry(self, placeholder_text="example@gmail.com")
         self.email_entry.grid(row=6, column=1, pady=(10, 0), padx=10, sticky="e")
 
+        # Dob label
         self.dob_label = customtkinter.CTkLabel(self, text="Date of Birth:")
         self.dob_label.grid(row=7, column=0, pady=(10, 0), padx=10, sticky="w")
 
@@ -178,14 +179,16 @@ class RegisterFrame(customtkinter.CTkFrame):
         self.register_button = customtkinter.CTkButton(self, text="Register", command=self.register_user)
         self.register_button.grid(row=8,column=0, pady=(10,0),padx=20, sticky="")
 
-
-
     def register_user(self):
+        """
+            Used to take all the credentials entered by the user and send to the client.RegisterDataSend Func 
+        """
         username = self.username_entry.get()
         name = self.name_entry.get()
         password = self.password_entry.get()
         email = self.email_entry.get()
         dob = self.dob_entry.get()
+
         if not username or not name or not password or not email or not dob:
             messagebox.showinfo("Error", "Please fill out all the field")
             return
@@ -206,7 +209,7 @@ class RegisterFrame(customtkinter.CTkFrame):
             elif check_register == INVAILD:
                 messagebox.showerror("Error", "Error ocurred Try after sometime")
                 logging.error("Return Invalid, Unexpected Error happened")
-                
+
     def switch_to_main(self):
         # Call the callback to switch back to the main frame
         self.switch_frame_callback("FirstSectionFrame")
@@ -216,38 +219,47 @@ class AfterLoginFrame(customtkinter.CTkFrame):
     def __init__(self, master, switch_frame_callback):
         super().__init__(master)
         self.switch_frame_callback = switch_frame_callback
-        recv_thread = threading.Thread(target=client.RecieveMessage, args=(self.on_message_recieved,ShutDownFlag,), daemon=True)
-        recv_thread.start()
+        global COUNT
+        print(COUNT)
+        if COUNT == 0:
+            print('thread excuted')
+            recv_thread = threading.Thread(target=client.RecieveMessage, args=(self.on_message_recieved,ShutDownFlag,), daemon=True)
+            recv_thread.start()
+            COUNT += 1
+        else:
+            COUNT += 1
+            print(f"recv thread not excuted count {COUNT}")
         app.geometry("700x600")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         # Default profile pic
-
         profile_image = Image.open("rc/unkown1.png")
         profile_image = profile_image.resize((20,20))
         profile_icon = ImageTk.PhotoImage(profile_image)
-
-        ## creating a search bar
-
+       
+        # search icon 
         search_image = Image.open("rc/search-icon.png")
         search_image = search_image.resize((10,10))
         search_icon = ImageTk.PhotoImage(search_image)
         
+        # search button with search icon
         self.search_button = customtkinter.CTkButton(self, image=search_icon, text="", width=0, command=self.search)
         self.search_button.place(relx=0.99, rely=0.02, anchor='ne')
 
+        # search bar
         self.search_bar_entry = customtkinter.CTkEntry(self, placeholder_text="Search User")
         self.search_bar_entry.place(relx=0.94, rely=0.02, anchor='ne')
 
-        self.profile_button = customtkinter.CTkButton(self, image=profile_icon, width=0, text="", fg_color="#444444")
+        # profile pic as button
+        self.profile_button = customtkinter.CTkButton(self, image=profile_icon, width=0, text="", fg_color="#444444", command=self.switch_to_EditProfileFrame)
         self.profile_button.place(relx=0.06, rely=0.02, anchor="ne")
 
-                # Message Displaying Screen
+        # Message Displaying Screen
         self.message_dis_box = customtkinter.CTkScrollableFrame(self, width=500, height=300)
         self.message_dis_box.grid(row=0, column=0, pady=(60, 0), padx=20, columnspan=2, sticky="nsew")
 
-        # Create a Text widget and Scrollbar directly inside the frame
+        # Text widget and Scrollbar directly inside the frame
         self.message_display = Text(self.message_dis_box, wrap="word", state="disabled", bg="#333333", foreground="white")
         self.message_display.pack(expand=True, fill="both", side="left")
 
@@ -257,22 +269,24 @@ class AfterLoginFrame(customtkinter.CTkFrame):
         # Attach scrollbar to the Text widget
         self.message_display.config(yscrollcommand=scrollbar.set)
 
-        # Message Entry and Send Button
+        # Message Entry 
         self.message_entry = customtkinter.CTkEntry(self, placeholder_text="Message")
         self.message_entry.grid(row=1, column=0, pady=(10, 10), padx=(20, 0), sticky="nsew")
 
+        # Send Button
         self.send_button = customtkinter.CTkButton(self, text="Send", width=20, command=self.send_normal_message)
         self.send_button.grid(row=1, column=1, pady=(10, 10), padx=(5, 20), sticky="nsew")
-
-
+        
     def start_recieve_thread(self,  ShutDownFlag):
         client.RecieveMessage(self.on_message_recieved, ShutDownFlag)
 
     def on_message_recieved(self, MessageContent):
         if MessageContent == KICK_CODE:
             logging.info("ADMIN KICK YOU OUT")
-            messagebox.showerror("ADMIN", "You been kicked out by the Admin")    
-            app.shutdown()  
+            messagebox.showerror("ADMIN", "You been kicked out by the Admin")   
+            print(ShutDownFlag) 
+            app.shutdown()
+            print(ShutDownFlag) 
         elif MessageContent == BAN_CODE:
             logging.info("ADMIN BANNED YOU FROM THE SERVER")
             messagebox.showerror("ADMIN", "You been Banned by the Admin")    
@@ -285,7 +299,11 @@ class AfterLoginFrame(customtkinter.CTkFrame):
             
     def send_normal_message(self):
         message = self.message_entry.get()
-        client.send_normal_message(message)
+        check = client.send_normal_message(message)
+        if check == SUCCESS:
+            pass
+        elif check == FAILED:
+            messagebox.showerror("Error", "Maybe server went offline/You been kicked out")
         self.message_entry.delete(0, 'end')
 
     def search(self):
@@ -296,7 +314,77 @@ class AfterLoginFrame(customtkinter.CTkFrame):
         elif check_search == FAILED:
             print(f"{username}")
             print("No user found")
-    
+    def switch_to_EditProfileFrame(self):
+        # Call the callback to switch to EditProfile
+        self.switch_frame_callback("EditProfileFrame")
+        logging.info("Switched to EditProfileFrame")
+
+class EditProfileFrame(customtkinter.CTkFrame):
+    def __init__(self, master, switch_frame_callback):
+        super().__init__(master)
+        self.switch_frame_callback = switch_frame_callback
+        app.geometry("600x500")
+
+        # Back icon
+        back_image = Image.open("rc/back-icon.png")
+        back_image = back_image.resize((10,10))
+        back_icon = ImageTk.PhotoImage(back_image)
+
+        # Back icon as button
+        self.back_button = customtkinter.CTkButton(self, image=back_icon,text="", border_width=0,fg_color="#333333",width=0, command=self.switch_to_AfterLoginFrame)
+        self.back_button.image = back_icon
+        self.back_button.grid(row=0, column=0, pady=(10, 0), sticky="w")
+
+        # Profile pic of the User
+        profile_image = Image.open("rc/unkown1.png")
+        profile_image = profile_image.resize((200,200))
+        profile_icon = ImageTk.PhotoImage(profile_image)
+
+        # Profile pic as a button
+        self.profile_button = customtkinter.CTkButton(self, image=profile_icon, width=0, text="", fg_color="#444444")
+        self.profile_button.place(relx=0.86, rely=0.2, anchor="ne")
+
+        # Label of the Username
+        self.username_label = customtkinter.CTkLabel(self, text="Username:")
+        self.username_label.grid(row=4, column=0, pady=(50,0), padx=10, sticky="w")
+
+        # Entry for the Username of the user
+        self.username_entry = customtkinter.CTkEntry(self)
+        self.username_entry.grid(row=4,column=1, pady=(50,0), padx=10, sticky="e")
+
+        # Label of the Name
+        self.name_label = customtkinter.CTkLabel(self, text="Name:")
+        self.name_label.grid(row=5, column=0, pady=(14,10), padx=10, sticky="w")
+
+        # Entry for the Name of the user
+        self.name_entry = customtkinter.CTkEntry(self)
+        self.name_entry.grid(row=5,column=1, pady=(14,10), padx=10, sticky="e")
+
+        # Label of the DOB
+        self.dob_label = customtkinter.CTkLabel(self, text="Date of Birth:")
+        self.dob_label.grid(row=6, column=0, pady=(6,10), padx=10, sticky="w")
+
+        # Entry for dob of the user
+        self.dob_entry = customtkinter.CTkEntry(self, placeholder_text="DD/MM/YY")
+        self.dob_entry.grid(row=6, column=1, pady=(6, 10), padx=10, sticky="e")
+
+        # Label of Email
+        self.email_label = customtkinter.CTkLabel(self, text="Email:")
+        self.email_label.grid(row=7, column=0, pady=(6, 10), padx=10, sticky="w")
+
+        # Entry for email of the User
+        self.email_entry = customtkinter.CTkEntry(self, placeholder_text="example@gmail.com")
+        self.email_entry.grid(row=7, column=1, pady=(6, 10), padx=10, sticky="e")
+
+        # Save button
+        self.save_button = customtkinter.CTkButton(self, text="Save" )
+        self.save_button.place(relx=0.439, rely=0.68, anchor="ne")
+
+    def switch_to_AfterLoginFrame(self):
+        # Call the callback to switch back to the AfterLogin
+        self.switch_frame_callback("AfterLoginFrame")
+        logging.info("Switched to AfterLogin")
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -310,11 +398,9 @@ class App(customtkinter.CTk):
         self.geometry("500x400")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-
         # Pass the self.show_frame as switch_frame_callback
         self.frame = FirstSectionFrame(self, self.show_frame)
         self.frame.grid(row=0, column=0, padx=20, pady=60, sticky="nsew")
-
         self.current_frame = None  # To keep track of the current frame
         self.show_frame("FirstSectionFrame")
         self.protocol("WM_DELETE_WINDOW",self.shutdown)
@@ -322,8 +408,10 @@ class App(customtkinter.CTk):
     def show_frame(self, frame_name):
         # Destroy the current frame (if any)
         if self.current_frame:
-            self.current_frame.destroy()
-
+            if frame_name == "AfterLoginFrame":
+                pass
+            else:
+                self.current_frame.destroy()
         # Switch to the specified frame
         if frame_name == "FirstSectionFrame":
             self.current_frame = FirstSectionFrame(self, self.show_frame)
@@ -333,15 +421,15 @@ class App(customtkinter.CTk):
             self.current_frame = LoginFrame(self, self.show_frame)
         elif frame_name == "AfterLoginFrame":
             self.current_frame = AfterLoginFrame(self, self.show_frame)
+        elif frame_name == "EditProfileFrame":
+            self.current_frame = EditProfileFrame(self, self.show_frame)
         self.current_frame.grid(row=0, column=0, padx=20, pady=60, sticky="nsew")
-
-
+        
     def shutdown(self):
         ShutDownFlag.set()
         client.shutdown_data_send()
         logging.info("Shuting Down the Application [+]  Done")        
         sys.exit()
-
 
 try:
     client = Client()
@@ -349,8 +437,6 @@ try:
     ShutDownFlag = threading.Event()
 except ConnectionError:
     Connected = False
-    
 
 app = App()
 app.mainloop()
-
